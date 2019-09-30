56c9c10e35a1553542b6b831a0acb2358f9dbdd9
Adam McChesney <amcchesney@taos.com>
2019-09-30 11:31:50 -0500

    updating to allow for either posting str or object

--- taosdevopsutils/slack.py
+++ taosdevopsutils/slack.py
@@ -9,12 +9,18 @@ class Slack:
         self.client = slack.WebClient(token=token)
 
     def _handle_webhook(self, target: str, message: str):
-        return requests.post(
-            target, data=message, headers={"Content-Type": "application/json"}
-        )
+        if isinstance(message, str):
+            return requests.post(
+                target, data=message, headers={"Content-Type": "application/json"}
+            )
+        return requests.post(target, json=message)
 
     def _handle_direct_message(self, target: str, message: str):
-        return self.client.chat_postMessage(channel=target, text=message, as_user=True)
+        if isinstance(message, str):
+            return self.client.chat_postMessage(
+                channel=target, text=message, as_user=True
+            )
+        return self.client.chat_postMessage(channel=target, as_user=True, **message)
 
     def post_slack_message(self, target: str, message: str):
         if target.startswith("https://"):
