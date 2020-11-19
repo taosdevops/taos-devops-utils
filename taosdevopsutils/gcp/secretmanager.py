97d3d3b47a0f15f8446d1f35f2cc6a327cfdfc66
Gretchen Shelby-Dormer <gshelbydormer@taos.com>
2020-11-19 13:26:45 -0500

    Update docstring

--- taosdevopsutils/gcp/secretmanager.py
+++ taosdevopsutils/gcp/secretmanager.py
@@ -2,7 +2,7 @@ from google.cloud import secretmanager
 
 
 def get_from_secret_manager(project_id:str, secret_name:str):
-    """Gets a secret from GCP Secret Manager.
+    """Gets the latest version of a secret from GCP Secret Manager.
     :param project_id: Name of GCP project where the secret lives.
     :type project_id: str
     :param secret_name: Name of the secret to access.
